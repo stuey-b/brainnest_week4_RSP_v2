926adafc6d3b79f1d222817a94675518f78fe903
stuey-b <stuartwbartlett@gmail.com>
2023-03-18 23:23:08 +0900

    remove switch statement. change heading variable name. add ternary operator.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const heading = document.getElementById(`heading`);
+const gameTitleDisplay = document.getElementById(`heading`);
 const choiceButtons = document.querySelectorAll(`.choice-buttons`);
 const playerChoiceDisplay = document.getElementById(`player-choice`);
 const computerChoiceDisplay = document.getElementById(`computer-choice`);
@@ -12,39 +12,25 @@ let playerSelection = ``;
 let computerSelection = ``;
 
 const computerPlay = () => {
-  switch (Math.floor(Math.random() * 3)) {
-    case 0:
-      computerSelection = `Rock`;
-      break;
-    case 1:
-      computerSelection = `Scissors`;
-      break;
-    case 2:
-      computerSelection = `Paper`;
-      break;
-  }
+  const computerChoices = [`Rock`, `Scissors`, `Paper`];
+  computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 };
 
 const compareChoices = () => {
   if (playerSelection === computerSelection) {
     resultsDisplay.textContent = `You TIED!`;
+  } else if (
+    (playerSelection === `Rock` && computerSelection === `Scissors`) ||
+    (playerSelection === `Paper` && computerSelection === `Rock`) ||
+    (playerSelection === `Scissors` && computerSelection === `Paper`)
+  ) {
+    resultsDisplay.textContent = `You WON!`;
+    playerScore++;
+    playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
   } else {
-    switch (playerSelection + " " + computerSelection) {
-      case `Paper Rock`:
-      case `Rock Scissors`:
-      case `Scissors Paper`:
-        playerScore++;
-        resultsDisplay.textContent = `You WON!`;
-        playerScoreDisplay.textContent = `Player score: ${playerScore}`;
-        break;
-      case `Rock Paper`:
-      case `Scissors Rock`:
-      case `Paper Scissors`:
-        computerScore++;
-        computerScoreDisplay.textContent = `Computer score: ${computerScore}`;
-        resultsDisplay.textContent = `You LOST!`;
-        break;
-    }
+    resultsDisplay.textContent = `You LOST!`;
+    computerScore++;
+    computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
   }
 };
 
@@ -56,29 +42,24 @@ const displayRoundResults = () => {
     computerChoiceDisplay.textContent = `Comp: ???`;
     resultsDisplay.textContent = ``;
     clearInterval(interval);
-  }, 2000);
+  }, 1500);
 };
 
 const declareWinner = () => {
-  if (playerScore === 5) {
-    heading.textContent = ` You WIN! You're the best, AROUND!`;
+  if (playerScore === 5 || computerScore === 5) {
+    playerScore === 5 ? gameTitleDisplay.textContent = `You WIN! You're the best, AROUND!` :
+      gameTitleDisplay.textContent = `OOF. You LOST. Try again`;
     const interval = setInterval(() => {
       gameReset();
       clearInterval(interval);
     }, 5000);
-  } else if (computerScore === 5) {
-    heading.textContent = `OOF. You LOST. Try again`;
-    const interval = setInterval(() => {
-      gameReset();
-      clearInterval(interval);
-    }, 5000);
-  }
+  };
 };
 
 const gameReset = () => {
   computerScore = 0;
   playerScore = 0;
-  heading.textContent = `ROCK! SCISSORS! PAPER!`;
+  gameTitleDisplay.textContent = `ROCK! SCISSORS! PAPER!`;
   resultsDisplay.textContent = `Reset! Let's Play`;
   playerScoreDisplay.textContent = `Player score: ${playerScore}`;
   computerScoreDisplay.textContent = `Computer score: ${computerScore}`;
@@ -87,8 +68,7 @@ const gameReset = () => {
 const game = () => {
   choiceButtons.forEach((choice) => {
     choice.addEventListener(`click`, (e) => {
-      const playerClicked = e.target.textContent;
-      playerSelection = playerClicked;
+      playerSelection = e.target.textContent;
       computerPlay();
       compareChoices();
       displayRoundResults();
